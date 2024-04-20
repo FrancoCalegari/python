4a329ff2e79b641fa9a5413b39e7a89e658223c7
Franco Calegari <francocalegari55@gmail.com>
2024-04-20 16:52:53 -0300

    csv

--- archivos/agregarTXT.py
+++ archivos/agregarTXT.py
@@ -1,4 +1,4 @@
 with open('archivos/info.txt', 'a', encoding="utf-8" ) as archivo:
     #esta parte sobreescribe el archivo
-    for i in range(5):
+    for i in range(5): #agregar varias lineas con rango for
         archivo.write(f"Hola como estan todos {i +1}\n")
